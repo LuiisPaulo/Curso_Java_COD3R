08bc236309cfc1d1540cccd190a3fe97d27dce77
Luis Paulo <luispaulolopesg@gmail.com>
2024-04-24 00:10:37 -0300

    Fix: primeiro commit repo via teste de arquivo

---
+++ Classe_Metodos/Teste.java
@@ -0,0 +1,10 @@
+package Curso_Java_COD3R.Classe_Metodos;
+
+import java.util.*;
+
+class teste {
+
+    public static void main(String[] args) {
+        System.out.println("Teste de implementação no github");
+    }
+}
